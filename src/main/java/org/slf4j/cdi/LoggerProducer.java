e097cd806ae4ee13efbcf13b912d2ec977432c2b
Patrick Reinhart <patrick@reini.net>
2017-10-21 23:03:32 +0200

    Initial commit

--- src/main/java/org/slf4j/cdi/LoggerProducer.java
+++ src/main/java/org/slf4j/cdi/LoggerProducer.java
@@ -1,8 +1,16 @@
 package org.slf4j.cdi;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class LoggerProducer {
-
+    @Produces
+    public static Logger createLogger(InjectionPoint ip) {
+        return LoggerFactory.getLogger(ip.getBean().getBeanClass());
+    }
 }
